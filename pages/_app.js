80ed107a1aba01acee66fe29e3d21fcf2e25b080
Yael Villarreal <iaeloko@gmail.com>
2022-09-17 23:37:50 -0500

    config ok

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,12 @@ import {getLibrary} from '../config/web3'
 import {Web3ReactProvider} from '@web3-react/core'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  
+  )
 }
 
 export default MyApp
